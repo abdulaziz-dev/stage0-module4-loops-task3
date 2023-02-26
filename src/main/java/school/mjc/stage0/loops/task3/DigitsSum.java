8bfe30c8bfb10d63b20457af6ef005fae49c7312
abdulaziz-dev <axmedovabdulaziz2001@gmail.com>
2023-02-26 11:07:29 +0500

    for loops

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        int sum = 0;
+        String son = t+"";
+        for (int i=0; i<son.length(); i++){
+            sum += son.charAt(i)-'0';
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        while(first != second){
+            if(first > second){
+                first = first - second;
+            } else {
+                second = second - first;
+            }
+        }
+        System.out.println(first);
     }
 }
