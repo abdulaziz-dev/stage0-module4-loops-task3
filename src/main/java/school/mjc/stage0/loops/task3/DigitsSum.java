aaf80a18f3ba3925e1bf46e3dd574513075eb994
abdulaziz-dev <axmedovabdulaziz2001@gmail.com>
2023-02-26 11:34:18 +0500

    for loops #2

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -3,6 +3,7 @@ package school.mjc.stage0.loops.task3;
 public class DigitsSum {
     public void printDigitsSum(int t){
         int sum = 0;
+        t = Math.abs(t);
         String son = t+"";
         for (int i=0; i<son.length(); i++){
             sum += son.charAt(i)-'0';
--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int f1 = 0;
+        int f2 = 1;
+        int f;
+        System.out.println(f1);
+        System.out.println(f2);
+        for(int i= 3; i<=lastFibonacci; i++){
+            f = f2 + f1;
+            f1 = f2;
+            f2 = f;
+            System.out.println(f);
+        }
     }
 }
